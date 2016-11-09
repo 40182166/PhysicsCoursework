932797543801fffa1416d9f221b37632aa2259b2
Scarfi <40182166@live.napier.ac.uk>
2016-11-09 13:52:37 +0000

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,104 @@
+#include "physics.h"
 #include <glm/glm.hpp>
 #include <glm/gtx/rotate_vector.hpp>
 #include <graphics_framework.h>
 #include <phys_utils.h>
+#include <thread>
 
 using namespace std;
 using namespace graphics_framework;
 using namespace glm;
 
-bool load_content() {
-	phys::Init();
-	return true;
+
+#define physics_tick 1.0 / 60.0
+
+static vector<unique_ptr<Entity>> SceneList;
+static unique_ptr<Entity> floorEnt;
+
+static vector<unique_ptr<Entity>> balls;
+
+float springConstant = 2.0f;
+float naturalLength = 2.0f;
+
+
+unique_ptr<Entity> CreateParticle() {
+	unique_ptr<Entity> ent(new Entity());
+	ent->SetPosition(vec3(0, 5.0 + (double)(rand() % 200) / 20.0, 0));
+	//ent->SetPosition(vec3(10.0, 10.0f, 10.0f));
+	unique_ptr<Component> physComponent(new cPhysics());
+	unique_ptr<cShapeRenderer> renderComponent(new cShapeRenderer(cShapeRenderer::SPHERE));
+	renderComponent->SetColour(phys::RandomColour());
+	ent->AddComponent(physComponent);
+	ent->AddComponent(unique_ptr<Component>(new cSphereCollider()));
+	ent->AddComponent(unique_ptr<Component>(move(renderComponent)));
+	return ent;
 }
 
-bool update(float delta_time) {
-	static float rot = 0.0f;
-	rot += 0.2f * delta_time;
-	phys::SetCameraPos(rotate(vec3(15.0f, 12.0f, 15.0f), rot, vec3(0, 1.0f, 0)));
+
+void CreateSpring(const vector<unique_ptr<Entity>> &particles, float originalLength, float k)
+{
+	vec3 force = particles[1]->GetPosition() - particles[0]->GetPosition();
+	float length = glm::length(force);
+	length = abs(length - originalLength) * k;
+
+	vec3 springNormal = normalize(force);
+	force = springNormal * (-length);
+	auto b = particles[0]->GetComponents("Physics");
+	if (b.size() == 1) {
+		const auto p = static_cast<cPhysics *>(b[0]);
+		p->forces -= force;
+	}
+
+	b = particles[1]->GetComponents("Physics");
+	if (b.size() == 1) {
+		const auto p = static_cast<cPhysics *>(b[0]);
+		p->forces += force;
+	}
+}
+
+
+bool update(double delta_time) {
+	static double t = 0.0;
+	static double accumulator = 0.0;
+	accumulator += delta_time;
+
+	while (accumulator > physics_tick) {
+		UpdatePhysics(t, physics_tick);
+		accumulator -= physics_tick;
+		t += physics_tick;
+	}
+
+	for (auto &e : SceneList) {
+		e->Update(delta_time);
+	}
+
+	CreateSpring(SceneList, naturalLength, springConstant);
+
 	phys::Update(delta_time);
 	return true;
 }
 
+bool load_content() {
+	phys::Init();
+	for (size_t i = 0; i < 2; i++) {
+		SceneList.push_back(move(CreateParticle()));
+	}
+
+	CreateSpring(SceneList, naturalLength, springConstant);
+
+	floorEnt = unique_ptr<Entity>(new Entity());
+	floorEnt->AddComponent(unique_ptr<Component>(new cPlaneCollider()));
+
+	phys::SetCameraPos(vec3(10.0f, 40.0f, -50.0f));
+	phys::SetCameraTarget(vec3(0.0f, 0.0f, 0));
+	InitPhysics();
+	return true;
+}
+
 bool render() {
-	phys::DrawSphere(glm::vec3(4.0f, 4.0f, 0), 1.0f, RED);
-	phys::DrawSphere(glm::vec3(-4.0f, 4.0f, 0), 1.0f, RED);
-	phys::DrawSphere(glm::vec3(0, 8.0f, 0), 0.2f, YELLOW);
-	phys::DrawSphere(glm::vec3(0), 1.0f, GREEN);
-	phys::DrawSphere(glm::vec3(0, 4.0f, 4.0f), 1.0f, BLUE);
-	phys::DrawSphere(glm::vec3(0, 4.0f, -4.0f), 1.0f, BLUE);
-	phys::DrawCube(glm::vec3(0, 4.0f, 0));
-	phys::DrawLine(glm::vec3(0, 4.0f, 4.0f), glm::vec3(0, 8.0f, 0));
-	phys::DrawLine(glm::vec3(0, 4.0f, -4.0f), glm::vec3(0, 8.0f, 0));
-	phys::DrawLine(glm::vec3(4.0f, 4.0f, 0), glm::vec3(0), true, ORANGE);
-	phys::DrawLine(glm::vec3(-4.0f, 4.0f, 0), glm::vec3(0), true, PINK);
-	phys::DrawLineCross(glm::vec3(0, 8.0f, 0), 1.0f, false);
-	phys::DrawArrow(glm::vec3(0, 4.0f, 0), glm::vec3(0, 8.0f, 0), 1.0f, GREY);
+	for (auto &e : SceneList) {
+		e->Render();
+	}
 	phys::DrawScene();
 	return true;
 }
@@ -48,3 +113,4 @@ void main() {
 	// Run application
 	application.run();
 }
+
